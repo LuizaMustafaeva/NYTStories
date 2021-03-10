2dc5e512beac84fcc34f0685c8735a6644cbea7b
LuizaMustafaeva <luiza.mustafaeva1997@gmail.com>
2021-03-10 22:10:01 +0500

    Added MainController

---
+++ NYTStories/Controllers/Cells/MainCell.swift
@@ -0,0 +1,26 @@
+//
+//  MainCell.swift
+//  NYTStories
+//
+//  Created by Luiza on 10.03.2021.
+//  Copyright © 2021 Luiza. All rights reserved.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NYTStories/Controllers/MainVC.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  NYTStories
+//
+//  Created by Luiza on 10.03.2021.
+//  Copyright © 2021 Luiza. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrayOfType = [""]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        arrayOfType = ["arts", "automobiles", "books", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"]
+        
+        settingsOfTableView()
+    }
+    
+    func settingsOfTableView(){
+
+        title = "Top Stories"
+        tableView.register(UINib.init(nibName: "MainCell", bundle: nil), forCellReuseIdentifier: "MainCell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension MainVC: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
+extension MainVC: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfType.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainCell", for: indexPath) as! MainCell
+        
+        let nameOfType = arrayOfType[indexPath.row]
+        
+        cell.nameLabel.text = nameOfType.capitalized
+        cell.nameLabel.font = UIFont(name: "TimesNewRomanPSMT", size: 22)
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+       
+        return cell
+    }
+}
+
+
